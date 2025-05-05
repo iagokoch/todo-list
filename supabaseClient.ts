21228d8bd10ed92b84c3ae925afa33d086e9962d
Iagokoch <iagon.koch@gmail.com>
2025-05-05 16:48:33 -0300

    backend com node e arrumando o tsconfig

--- supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://hautzavvfajzozwxofwr.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
